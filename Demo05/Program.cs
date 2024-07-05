153f882be4587de53b4175ab2136c2ea5d3f8bd0
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-07-05 05:42:10 -0400

    Boxing, Unboxing

--- Demo05/Program.cs
+++ Demo05/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Session 05
+            #region Casting [Boxing - Unboxing]
+            //Boxing => casting from value type to reference type (safe)
+            //Unboxing => casting from reference type to value type (unsafe)
+
+            #region EX01
+            object O1;
+            O1 = "Ahmed";
+            O1 = 1; // Casting [Value type -> Reference type] Boxing
+            O1 = 'A'; // Casting [Value type -> Reference type] Boxing
+            O1 = 1.5; // Casting [Value type -> Reference type] Boxing 
+            #endregion
+
+            #region EX02
+
+            int X = 10;
+            object Obj = X; // Boxing (safe casting)
+            // Obj = new object(10);
+            // Parent => Child 
+            // Animal => Dog
+
+            object Obj2 = 10;
+            int Y = (int)Obj2; // Casting Unboxing (unsafe casting)
+            //New object(10) => 10
+            // Dog => Animal (Animal is a dog)
+
+            Console.WriteLine(Y);
+
+            #endregion
+
+            object obj = 1; // Boxing 
+            // we dont use it anymore because of the performance and memory issue
+            // we use generics instead of boxing and unboxing
+
+            #endregion
         }
     }
 }
