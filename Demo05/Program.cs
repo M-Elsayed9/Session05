c2524fb4a5a24925571a39036f8e0d34ea9ca70f
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-07-05 22:45:52 -0400

    Nullable Type - Value Type

--- Demo05/Program.cs
+++ Demo05/Program.cs
@@ -10,33 +10,102 @@
             //Unboxing => casting from reference type to value type (unsafe)
 
             #region EX01
-            object O1;
-            O1 = "Ahmed";
-            O1 = 1; // Casting [Value type -> Reference type] Boxing
-            O1 = 'A'; // Casting [Value type -> Reference type] Boxing
-            O1 = 1.5; // Casting [Value type -> Reference type] Boxing 
+            //object O1;
+            //O1 = "Ahmed";
+            //O1 = 1; // Casting [Value type -> Reference type] Boxing
+            //O1 = 'A'; // Casting [Value type -> Reference type] Boxing
+            //O1 = 1.5; // Casting [Value type -> Reference type] Boxing 
+            //#endregion
+
+            //#region EX02
+
+            //int X = 10;
+            //object Obj = X; // Boxing (safe casting)
+            //// Obj = new object(10);
+            //// Parent => Child 
+            //// Animal => Dog
+
+            //object Obj2 = 10;
+            //int Y = (int)Obj2; // Casting Unboxing (unsafe casting)
+            ////New object(10) => 10
+            //// Dog => Animal (Animal is a dog)
+
+            //Console.WriteLine(Y);
+
+            #endregion
+
+            /*object obj = 1;*/ // Boxing 
+                                // we dont use it anymore because of the performance and memory issue
+                                // we use generics instead of boxing and unboxing
+
+            #endregion
+
+            #region Nullable Type
+
+            // Null as a value is valid only with reference type
+
+            #region Value Type C# 2.0 2005
+
+            #region EX01
+            // int X = null; // Error because int is a value type (non nullable)
+            // int X => X allow only int numbers
+
+            //int? Y = null; // Nullable type (int?) allow null as a value
+            //int X = 10;
+            //int? Z = Y; // Z = null
+            //int? Z2 = X; // Z2 = 10
+            //int? Z3 = Z;
+
+            ////int? Num; 
+            ////Nullable<int> Num; // Nullable is a struct that allow null as a value
+
+            //decimal Num01 = 1m;
+            //decimal? Num02 = Num01; // works
+
+            //decimal? Num03 = Num02;
+            // Num01 = Num02; does not work  
             #endregion
 
             #region EX02
-
             int X = 10;
-            object Obj = X; // Boxing (safe casting)
-            // Obj = new object(10);
-            // Parent => Child 
-            // Animal => Dog
+            int? Y = X; //Valid implicit casting
 
-            object Obj2 = 10;
-            int Y = (int)Obj2; // Casting Unboxing (unsafe casting)
-            //New object(10) => 10
-            // Dog => Animal (Animal is a dog)
+            int? Num01 = null;
+            int Num02 = (int)Num01; // Explicit casting (unsafe casting)
+            //invalid operartion => exception
 
-            Console.WriteLine(Y);
+            int? K = null;
+            int L;
+            L = (int)K; // (unsafe)
+
+            if (K != null)
+            {
+                L = (int)K; //Casting safe
+            }
+            else
+            {
+                L = 0;
+            }
+
+            // or
+
+            if (K.HasValue)
+            {
+                L = K.Value; // Not Casting (safe)
+            }
+            else
+            {
+                L = 0;
+            }
+
+            //or 
+
+            L = K.HasValue ? K.Value : 0;
+            L = L != null ? (int)K : 0;
 
             #endregion
 
-            object obj = 1; // Boxing 
-            // we dont use it anymore because of the performance and memory issue
-            // we use generics instead of boxing and unboxing
+            #endregion
 
             #endregion
         }
