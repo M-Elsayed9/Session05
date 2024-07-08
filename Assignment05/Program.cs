2f46274647cb2dbf35f3454536ce5cb52832f98b
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-07-07 21:06:57 -0400

    Questions 01

--- Assignment05/Program.cs
+++ Assignment05/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace Assignment05
+﻿using System.ComponentModel;
+using System.Data.Common;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace Assignment05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            //1 - Explain the difference between passing(Value type parameters)
+            //by value and by reference then write a suitable c# example.
+
+            // By Value
+            // When a value type parameter is passed by value, a copy of the variable
+            // is passed to the method. Changes made to the parameter inside the method
+            // do not affect the original variable.
+
+            //By Reference
+            //When a value type parameter is passed by reference using the ref or out
+            //keyword, the method receives a reference to the actual variable.
+            //Changes made to the parameter inside the method do affect the original variable.
+
+            #endregion
+
+            #region Question 2
+
+            //2 - Explain the difference between passing(Reference type parameters)
+            //by value and by reference then write a suitable c# example.
+
+            
+            
+            #endregion
+
+            #region Question 3
+
+
+
+            #endregion
+
+            #region Question 4
+
+            #endregion
+
+            #region Question 5
+
+            #endregion
+
+            #region Question 6
+
+            #endregion
+
+            #region Question 7
+
+            #endregion
+
+            #region Question 8
+
+            #endregion
         }
     }
 }
